534385b6881fd0c22fec3cea86303cd9d3432755
olalekan <olamilake95@gmail.com>
2024-08-30 20:40:51 +0100

    config

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Assuming 'index.html' is in the root directory
+    },
+  },
 })
